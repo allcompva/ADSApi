e56f959473d6c01becbb3a065f1628aedf9e0fbd
Ignacio Martin Velez <ignacio.velez@synapticlinks.com.ar>
2024-12-11 18:35:22 -0300

    Formulario terminado

--- obj/Release/net6.0/ADSWebAPI.AssemblyInfo.cs
+++ obj/Release/net6.0/ADSWebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ADSWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd0ed1bb222688f0de487a87b8c1cab9dfa0350f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d007f5ef939910cd4942dc9c4ee20ace332501b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ADSWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ADSWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
